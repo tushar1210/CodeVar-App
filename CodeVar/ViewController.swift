222a26af7c6ffff2fe904a4e94aed8605bcf987b
tushar1210 <ts26192271@gmail.com>
2019-10-09 13:02:41 +0530

    initial

---
+++ CodeVar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeVar
+//
+//  Created by Tushar Singh on 09/10/19.
+//  Copyright © 2019 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
