ea04cffd560f43c920a464447503a9756352641c
Ankit Yadav <yadavankit840@gmail.com>
2019-10-12 19:45:49 +0530

    timeline initiated.

---
+++ CodeVar/profileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  profileViewController.swift
+//  CodeVar
+//
+//  Created by ANKIT YADAV on 12/10/19.
+//  Copyright © 2019 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+         self.navigationController?.isNavigationBarHidden = false
+    }
+    
+
+}
